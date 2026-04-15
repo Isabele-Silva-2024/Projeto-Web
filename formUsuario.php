df9f2069868235e8ab904632105c155b86bf7a86
Isabele-Silva-2024 <iisabelesilva123@gmail.com>
2026-04-15 14:04:25 -0300

    Create formUsuario.php

---
+++ formUsuario.php
@@ -0,0 +1,68 @@
+<?php include "header.php" ?>
+    <div class="d-flex justify-content-center mb-3">
+        <h2>Cadastre-se:</h2>
+    </div>
+
+    <div class="d-flex justify-content-center mb-3">
+        <form action="actionUsuario.php" method="POST" class="was-validated">
+            
+            <div class="form-floating mt-3 mb-3">
+                <input type="file" name="fotoUsuario" id="fotoUsuario" placeholder="Foto" class="form-control" >
+                <label for="fotoUsuario">Foto</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="text" name="nomeUsuario" id="nomeUsuario" placeholder="Nome Completo" class="form-control" >
+                <label for="nomeUsuario">Nome completo</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="date" name="dataNascimentoUsuario" id="dataNascimentoUsuario" placeholder="Data de nasciemnto" class="form-control" >
+                <label for="dataNascimentoUsuario">Data de nasciemento</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <select name="cidadeUsuario" id="cidadeUsuario" placeholder="Cidade" class="form-control" >
+                    <option value="Curiúva">Curiúva</option>  
+                    <option value="Imbaú">Imbaú</option> 
+                    <option value="Ortigueira">Ortigueira</option> 
+                    <option value="Reserva">Reserva</option> 
+                    <option value="Telêmaco Borba" selected>Telêmaco Borba</option> 
+                    <option value="Tibagi">Tibagi</option> 
+                </select>
+                <label for="cidadeUsuario">Cidade</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="email" name="emailUsuario" id="emailUsuario" placeholder="email" class="form-control" >
+                <label for="emailUsuario">Email</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="senhaUsuario" id="senhaUsuario" placeholder="senha" class="form-control" minlength="6" maxlength="10" >
+                <label for="senhaUsuario">Crie sua senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <div class="form-floating mt-3 mb-3">
+                <input type="password" name="confirmarSenhaUsuario" id="confirmarSenhaUsuario" placeholder=" Confirme a senha" class="form-control" minlength="6" maxlength="10" >
+                <label for="senhaUsuario">Confirme a senha</label>
+                <div class="valid-feedback"></div>
+                <div class="invalid-feedback"></div>
+            </div>
+
+            <button type="submit" class="btn btn-outline-dark">Cadastrar</button>
+        </form>
+    </div>
+<?php include "footer.php" ?>
