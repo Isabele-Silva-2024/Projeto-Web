b393bb6dfb025ae9a432e39471ef35091f2734d9
Isabele-Silva-2024 <iisabelesilva123@gmail.com>
2026-04-15 14:03:59 -0300

    Create actionUsuario.php

---
+++ actionUsuario.php
@@ -0,0 +1,150 @@
+<?php include "header.php" ?>
+    <?php
+        //Verificar se o método de envio das informações do form é "POST"
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            //Cria variáveis para armazenar informações recebidas do array $_POST
+            $fotoUsuario = $nomeUsuario = $dataNascimentoUsuario = $cidadeUsuario = $emailUsuario = $senhaUsuario = $confirmarSenhaUsuario = "";
+
+            //Variável booleana para controle de erros de preenchimento
+            $erroPreenchimento = false;
+
+            //Validação do campo nomeUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if (empty($_POST["nomeUsuario"])){
+                echo "<div class='alert alert-warning text-center'> O campo <strong>NOME</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }   
+            else{
+                //Se o $_POST["nomeUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $nomeUsuario = filtrar_entrada($_POST["nomeUsuario"]);
+            }
+
+            //Validação do campo dataNascimentoUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if (empty($_POST["dataNascimentoUsuario"])){
+                echo "<div class='alert alert-warning text-center'> O campo <strong>DATA DE NASCIMENTO</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }   
+            else{
+                //Se o $_POST["dataNascimentoUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $dataNascimentoUsuario = filtrar_entrada($_POST["dataNascimentoUsuario"]) ;
+
+                //Utiliza a função strlen() para verificar o comprimento da string $dataNascimentoUsuario
+                if(strlen($dataNascimentoUsuario)==10){
+                    //Aplicar a função substr() para gerar substrings e armazenar dia, mês e ano de nascimento
+                    $diaNascimentoUsuario = substr($dataNascimentoUsuario, 8, 2);
+                    $mesNascimentoUsuario = substr($dataNascimentoUsuario, 5, 2);
+                    $anoNascimentoUsuario = substr($dataNascimentoUsuario, 0, 4);
+                }
+                else{
+                    echo "<div class= 'alert alert-warning text-center'><strong>DATA</strong>inválida!</div>";
+                    $erroPreenchimento = true;
+                }
+            }
+
+            //Validação do campo cidadeUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if (empty($_POST["cidadeUsuario"])){
+                echo "<div class='alert alert-warning text-center'> O campo <strong>CIDADE</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }   
+            else{
+                //Se o $_POST["cidadeUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $cidadeUsuario = filtrar_entrada($_POST["cidadeUsuario"]) ;
+            }
+
+            //Validação do campo emailUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if (empty($_POST["emailUsuario"])){
+                echo "<div class='alert alert-warning text-center'> O campo <strong>EMAIL</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }   
+            else{
+                //Se o $_POST["emailUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                $emailUsuario = filtrar_entrada($_POST["emailUsuario"]) ;
+            }
+
+            //Validação do campo senhaUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+            if(empty($_POST["senhaUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>SENHA</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["senhaUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                //Usa a função md5() para criptografar a senha do usuário
+                $senhaUsuario = md5(filtrar_entrada($_POST["senhaUsuario"]));
+            }
+
+            //Validação do campo confirmarSenhaUsuario
+            //Utiliza a função empty() para verificar se o campo está vazio
+             if(empty($_POST["confirmarSenhaUsuario"])){
+                echo "<div class='alert alert-warning text-center'>O campo <strong>CONFIRMAR SENHA</strong> é obrigatório!</div>";
+                $erroPreenchimento = true;
+            }
+            else{
+                //Se o $_POST["confirmarSenhaUsuario"] não estiver vazio, é filtrado e armazenado na variável PHP
+                //Usa a função md5() para criptografar a senha do usuário
+                $confirmarSenhaUsuario = md5(filtrar_entrada($_POST["confirmarSenhaUsuario"]));
+
+                //Compara se as senhas são diferentes
+                if($senhaUsuario!=$confirmarSenhaUsuario){
+                    echo "<div class='alert alert-warning text-center'>As <strong>SENHAS</strong> informadas não são iguais!</div>";
+                    $erroPreenchimento = true;
+                }
+            }
+
+            //Verifica se não há erro de preenchimento
+            if (!$erroPreenchimento){
+                echo "<div class='alert alert-success text-center'> O cadastro do <strong>USUÁRIO</strong> foi efetuado com sucesso!</div>";
+                echo "
+                    <div class='container mb-3 mt-3'>
+                        <table class='table'>
+                            <tr>
+                                <th>NOME</th>
+                                <td>$nomeUsuario</td>
+                            </tr>
+                            <tr>
+                                <th>DATA DE NASCIMENTO</th>
+                                <td>$diaNascimentoUsuario/$mesNascimentoUsuario/$anoNascimentoUsuario</td>
+                            </tr>
+                            <tr>
+                                <th>CIDADE</th>
+                                <td>$cidadeUsuario</td>
+                            </tr>
+                            <tr>
+                                <th>EMAIL</th>
+                                <td>$emailUsuario</td>
+                            </tr>
+                            <tr>
+                                <th>SENHA</th>
+                                <td>$senhaUsuario</td>
+                            </tr>
+                            <tr>
+                                <th>CONFIRMAR SENHA</th>
+                                <td>$confirmarSenhaUsuario</td>
+                            </tr>
+                        </table>
+                    </div>
+                ";
+            }
+            else{
+                echo "<div class='alert alert-danger text-center'> Erro ao tentar efetuar cadastro do <strong>USUÁRIO</strong>!</div>";
+            }
+        }
+        else{
+            //Usar a função header() para redirecionar o usuário para o formulário.php          
+            header("location:formUsuario.php");
+        }
+
+        //Função para filtrar entrada de4 dados e enviar SQL Injection
+        function filtrar_entrada($dado){
+            $dado= trim($dado); //Remove espaços desnecessários
+            $dado= stripslashes($dado); //Remove barras invertidas
+            $dado= htmlspecialchars($dado); //Converte caracteres especiais em entidades HTML
+
+            //Após o dado passar pelos filtros, é retornado
+            return($dado);
+        }
+    ?>
+<?php include "footer.php" ?>
