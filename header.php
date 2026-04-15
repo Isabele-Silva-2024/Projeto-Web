8de5b7fea4a7b099512198a6d48b4fffed04873b
Isabele-Silva-2024 <iisabelesilva123@gmail.com>
2026-04-15 14:06:25 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>UniMarket - Unindo pessoas, produtos e possibilidades.</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- CDN para Ícones do Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- CSS dos temas do Template-->
+        <link href="css/styles.css" rel="stylesheet" />
+
+        <!--CDN da fonte do Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
+    
+        <!-- CSS da Fonte fo Slogan --> 
+        <style>
+            .audiowide-regular {
+                font-family: "Audiowide", sans-serif;
+                font-weight: 400;
+                font-style: normal;
+                front-size: 2 rem;
+            }
+        </style>
+
+    </head>
+    <body>
+        <!-- Barra de navegação do sistema-->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="index.php" title="Retornar para a página inicial">UniMarket</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php" title="Retornar para a página inicial">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Sobre</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Loja</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">Todos os Produtos</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Itens Populares</a></li>
+                                <li><a class="dropdown-item" href="#!">Novidades</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Carrinho
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                        </button>
+                    </form>
+                    <ul class="navbar-nav mb2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link" href="formLogin.php">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Cabeçalho-->
+        <header class="bg-dark py-2">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <img src= "assets/img/Logo_Unimarket_Branco.png" style="width:150px" class="pb-3">
+                    <p class="lead fw-normal text-white mb-0 audiowide-regular">Unindo pessoas, produtos e possibilidades</p>
+                </div>
+            </div>
+        </header>
+        <!-- Seção Principal-->
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                
